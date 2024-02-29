6530a266ce2753bcd895084df77c25c3cffe0a56
mmajudith <mmajudith2000@gmail.com>
2024-02-28 21:37:13 -0800

    feat
    Styled the modal box

--- app.js
+++ app.js
@@ -16,23 +16,22 @@ bookBTN.addEventListener('click', (e) => {
 	e.preventDefault();
 	const fields = document['form'].elements;
 	const { fullName, email, phoneNumber, datetime, reason } = fields;
-	console.log(fullName.value);
 
-	if (/^[^\s]+( [^\s]+)+$/.test(fullName.value)) {
-		handleModalBox('#f46f74', 'Please enter valid full name');
+	if (!/^[^\s]+( [^\s]+)+$/.test(fullName.value)) {
+		return handleModalBox('#f46f74', 'Please enter valid full name');
 	}
-	if (/^[^ ]+@[^ ]+\.[a-z]{2,63}$/.test(email.value)) {
-		handleModalBox('#f46f74', 'Please enter valid email address.');
+	if (!/^[^ ]+@[^ ]+\.[a-z]{2,63}$/.test(email.value)) {
+		return handleModalBox('#f46f74', 'Please enter valid email address.');
 	}
-	if (/[0-9]{9,15}/.test(phoneNumber.value)) {
-		handleModalBox('#f46f74', 'Please enter valid phone number');
+	if (!/[0-9]{9,15}/.test(phoneNumber.value)) {
+		return handleModalBox('#f46f74', 'Please enter valid phone number');
 	}
-	// if (/^[^\s]+( [^\s]+)+$/.test(datetime)) {
-	// 	handleModalBox('#f46f74', 'Please enter valid full name');
+	// if (!/^[^\s]+( [^\s]+)+$/.test(datetime)) {
+	// 	return handleModalBox('#f46f74', 'Please enter valid full name');
 	// }
-	if (/^\S.*(?:\r?\n\S.*)*$/u.test(reason.value)) {
-		handleModalBox('#f46f74', 'Please enter valid full name');
+	if (!/^\S.*(?:\r?\n\S.*)*$/u.test(reason.value)) {
+		return handleModalBox('#f46f74', 'Please state your reason/purpose');
 	}
 
-	handleModalBox('#25CED1', 'Appointment successfully booked.');
+	return handleModalBox('#25CED1', 'Appointment successfully booked.');
 });
